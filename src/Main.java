11b9efad75178dced35a1e3d4cb7e4f8cf7489c6
Linke <linke@duck.com>
2023-02-17 04:57:10 +0300

    task 2 Completed fixed task 1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
-    public static void task1(){
+    public static void task1() {
         byte a = 10;
         short b = 1027;
         int c = -123456;
-        long d = -4287356;
+        long d = -4_287_356L;
         float e = 5.83f;
         double f = 16.23856712;
         System.out.println(a);
@@ -15,5 +16,21 @@ public class Main {
         System.out.println(d);
         System.out.println(e);
         System.out.println(f);
+    }
+    public static void task2() {
+        float a = 27.12f;
+        long b = 987_678_965_549L;
+        double c = 2.786;
+        short d = 567;
+        short e = -159;
+        int f = 27897;
+        byte g = 67;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
     }
 }
