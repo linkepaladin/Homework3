7ba2e3c4fee39c7e0fc13e4bdafb2a0ccc60e38f
Linke <linke@duck.com>
2023-02-17 04:43:01 +0300

    task 1 Completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1(){
+        byte a = 10;
+        short b = 1027;
+        int c = -123456;
+        long d = -4287356;
+        float e = 5.83f;
+        double f = 16.23856712;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
     }
 }
