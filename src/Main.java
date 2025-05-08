ef2bd4fbc0a76fbcfb14c8384d8d13fd75a3210c
linke <linke@duck.com>
2025-05-08 14:42:29 +0300

    Complete Task2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1() {
         int age = 17;
@@ -8,6 +9,15 @@ public class Main {
             System.out.println("Если возраст человека " + age + ", то он совершеннолетний");
         } else {
             System.out.println("Человек не достиг совершеннолетия, надо немного подождать");
+        }
+    }
+    public static void  task2() {
+        int temp = 3;
+        if (temp > 5) {
+            System.out.println("На улице " + temp + " градусов, можно идти без шапки");
+        }
+        if (temp < 5) {
+            System.out.println("На улице " + temp + " градусов, нужно надеть шапку");
         }
     }
 }
